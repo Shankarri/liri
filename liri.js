37a62f98db3a2a3f528b690fe190532de806319b
LAPTOP-RQ4QRHU8\aidan <aidanclemente@gmail.com>
2018-01-29 11:47:31 -0500

    unknown command not working properly, readme updated

--- liri.js
+++ liri.js
@@ -13,7 +13,6 @@ var searchValue = "";
 for (var i = 3; i < process.argv.length; i++) {
     searchValue += process.argv[i] + " ";
 };
- console.log("Search Value: ", searchValue); 
 
 // Error Functions 
 function errorFunction(respError) {
@@ -24,22 +23,17 @@ function errorFunction(respError) {
 
 // For logging to log.txt
 function errorFunctionStart (respError) {
-    if (respError) {
-        console.log(respError);
-      } else {
-        console.log("Log Started!\n");
-      }
+    errorFunction();
+    console.log("==== Log Started ====");
+
 };
 
 function errorFunctionEnd (respError) {
-    if (respError) {
-        console.log(respError);
-      } else {
-        console.log("Log Ended!");
-      }
+    errorFunction();
+    console.log("==== Log Ended ====");
 };
 
-// ++++++++++++++++ Twitter my-tweets ++++++++++++++++++++++
+// -------------------- Twitter my-tweets ----------------------------
 function getTweets() {
     // Accesses Twitter Keys
     var client = new Twitter(keys.twitter); 
@@ -54,7 +48,7 @@ function getTweets() {
 
         fs.appendFile("log.txt", "-----Tweets Log Entry Start-----\n\nProcessed at: \n" + Date() + "\n\n" + "terminal commands: \n" + process.argv + "\n\n" + "Data Output: \n", errorFunctionStart());
 
-        console.log("-------------------------- Aidan's Tweets -----------------------------\n");
+        console.log("\n-------------- Aidan's Tweets --------------\n");
         for (i = 0; i < tweets.length; i++) {
             console.log(i + 1 + ". Tweet: ", tweets[i].text);
 
@@ -67,13 +61,13 @@ function getTweets() {
             
             fs.appendFile("log.txt", (i + 1 + ". Tweet: ", tweets[i].text + "\nTweeted on: " + tweets[i].created_at + "\n\n"), errorFunction());
         };
-        console.log("-----------------------------------------------------------------------\n");
+        console.log("--------------------------------------------------\n");
 
         fs.appendFile("log.txt", "-----Tweets Log Entry End-----\n\n", errorFunctionEnd());
     });
 };
 
-// ++++++++++++++++++ Spotify spotify-this-song ++++++++++++++++++++++++++++
+// ======================= Spotify spotify-this-song ============================
 function searchSong(searchValue) {
 
     if (searchValue == "") {
@@ -91,12 +85,12 @@ function searchSong(searchValue) {
 
          var songResp = response.tracks.items;
 
-         console.log("\n----------- Spotify Search Result ------------------\n");
+         console.log("\n=============== Spotify Search Result ===============\n");
          console.log(("Artist: " + songResp[0].artists[0].name));
          console.log(("Song title: " + response.tracks.items[0].name));
          console.log(("Album name: " + songResp[0].album.name));
          console.log(("URL Preview: " + songResp[0].preview_url));
-         console.log("\n------------------------------------------------------\n");
+         console.log("\n=====================================================\n");
 
          fs.appendFile("log.txt", "Artist: " + songResp[0].artists[0].name + "\nSong title: " + response.tracks.items[0].name + "\nAlbum name: " + songResp[0].album.name + "\nURL Preview: " + songResp[0].preview_url + "\n\n-----Spotify Log Entry End-----\n\n", errorFunctionEnd());
      })
@@ -120,7 +114,7 @@ function searchMovie(searchValue) {
         if (!respError && response.statusCode === 200) {
             movieBody = JSON.parse(body);
             
-            console.log("\n------------- OMDB Search Results ------------------\n");
+            console.log("\n++++++++++++++++ OMDB Search Results ++++++++++++++++\n");
             console.log("Movie Title: " + movieBody.Title);
             console.log("Year: " + movieBody.Year);
             console.log("IMDB rating: " + movieBody.imdbRating);
@@ -135,14 +129,14 @@ function searchMovie(searchValue) {
             console.log("Language: " + movieBody.Language);
             console.log("Plot: " + movieBody.Plot);
             console.log("Actors: " + movieBody.Actors);
-            console.log("\n----------------------------------------------------\n");
+            console.log("\n+++++++++++++++++++++++++++++++++++++++++++++++++\n");
 
             fs.appendFile("log.txt", "Movie Title: " + movieBody.Title + "\nYear: " + movieBody.Year + "\nIMDB rating: " + movieBody.imdbRating + "\nRotten Tomatoes Rating: " + movieBody.Ratings[[1]].Value + "\nCountry: " + movieBody.Country + "\nLanguage: " + movieBody.Language + "\nPlot: " + movieBody.Plot + "\nActors: " + movieBody.Actors + "\n\n-----OMDB Log Entry End-----\n\n", errorFunctionEnd());
         }
     });
 };
 
-//+++++++++++++++++ Random do-what-it-says +++++++++++++++++++++++++
+// xxxxxxxxxxxxxxxxxxxxxxxxxxx Random do-what-it-says xxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 function randomSearch() {
 
     fs.readFile("random.txt", "utf8", function(respError, data) {
@@ -178,7 +172,7 @@ switch (command) {
         break;
 };
 
-//------------- If the user enters an undefinded command ---------------------------
+// **************** If the user enters an undefinded command ************************
 var unknownCommand = "";
 
 for (var i = 2; i < process.argv.length; i++) {
@@ -189,5 +183,5 @@ console.log("Unknown Command: ", unknownCommand);
 
 //This is not working correctly: it is logging with the defined commands
 if ((unknownCommand != "do-what-it-says") || (unknownCommand != "movie-this") || (unknownCommand != "spotify-this-song") || (unknownCommand != "my-tweets")) {
-    console.log("I'm sorry, " + unknownCommand + " is not a command that I recognize. Please try one of the following commands: \n For a random search: node liri.js do-what-it-says \n To search a movie title: node liri.js movie-this (with a movie title following) \n To search Spotify for a song: node liri.js spotify-this-song (with a song title following) \n To see the last 20 of Aidan Clemente's tweets on Twitter: node liri.js my-tweets \n");
+    console.log("I'm sorry, " + unknownCommand + " is not a command that I recognize. Please try one of the following commands: \n\n  1. For a random search: node liri.js do-what-it-says \n\n  2. To search a movie title: node liri.js movie-this (with a movie title following) \n\n  3. To search Spotify for a song: node liri.js spotify-this-song (with a song title following) \n\n  4. To see the last 20 of Aidan Clemente's tweets on Twitter: node liri.js my-tweets \n");
 }
